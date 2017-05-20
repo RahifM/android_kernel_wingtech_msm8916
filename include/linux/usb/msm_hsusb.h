90082c7c14f32d6b63d8408c7b2ac9f87f76b21d
Balázs Triszka <balika011@gmail.com>
2017-05-20 19:06:14 +0000

    drivers: usb: Increase charge current
    Signed-off-by: RahifM <faizel326@gmail.com>

--- include/linux/usb/msm_hsusb.h
+++ include/linux/usb/msm_hsusb.h
@@ -119,7 +119,7 @@ enum msm_usb_phy_type {
 #define IDEV_CHG_MIN	500
 #define IUNIT		100
 
-#define IDEV_ACA_CHG_MAX	1500
+#define IDEV_ACA_CHG_MAX	2000
 #define IDEV_ACA_CHG_LIMIT	500
 
 #else
@@ -127,7 +127,7 @@ enum msm_usb_phy_type {
 #define IDEV_CHG_MIN	500
 #define IUNIT		100
 
-#define IDEV_ACA_CHG_MAX	1500
+#define IDEV_ACA_CHG_MAX	2000
 #define IDEV_ACA_CHG_LIMIT	500
 #endif
 #define IDEV_HVDCP_CHG_MAX	1800
