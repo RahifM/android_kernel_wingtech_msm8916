3fd7d070ac1a03bc4edd354e47cecea112861bc1
RahifM <faizel326@gmail.com>
2020-02-10 15:20:16 +0530

    build: GCC-9 bruh!
    Signed-off-by: RahifM <faizel326@gmail.com>

--- build.sh
+++ build.sh
@@ -27,7 +27,7 @@ yellow='\033[0;33m'
 red='\033[0;31m'
 nocol='\033[0m'
 # Modify the following variable if you want to build
-export CROSS_COMPILE="$HOME/.kernel/gcc-arm-8.2-2018.08-x86_64-arm-linux-gnueabi/bin/arm-linux-gnueabi-"
+export CROSS_COMPILE="$HOME/android/kernels/toolchain/gcc-arm-9.2-2019.12-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-"
 export ARCH=arm
 export SUBARCH=arm
 make wt88047_defconfig
